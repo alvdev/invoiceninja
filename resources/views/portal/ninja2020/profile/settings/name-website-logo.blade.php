a97c8c30b744c4f5fea55f14cb9eed5ddbd7158b
alv.dev <soy@alv.dev>
2023-10-02 09:34:07 +0200

    feat: add branded filling profile page

--- resources/views/portal/ninja2020/profile/settings/name-website-logo.blade.php
+++ resources/views/portal/ninja2020/profile/settings/name-website-logo.blade.php
@@ -1,68 +1,69 @@
-<div class="mt-10 sm:mt-6">
-    <div class="md:grid md:grid-cols-3 md:gap-6">
+<div class="mt-8">
+    <div>
         <div class="md:col-span-1">
             <div class="sm:px-0">
-                <h3 class="text-lg font-medium leading-6 text-gray-900">{{ ctrans('texts.client_details') }}</h3>
+                <h3 class="text-3xl font-semibold leading-6 text-gray-900">{{ ctrans('texts.client_details') }}</h3>
             </div>
         </div> <!-- End of left side -->
 
-        <div class="mt-5 md:mt-0 md:col-span-2">
+        <div class="mt-8">
             <form wire:submit.prevent="submit" method="POST" id="update_contact">
                 @csrf
-                <div class="shadow overflow-hidden rounded">
-                    <div class="px-4 py-5 bg-white sm:p-6">
-                        <div class="grid grid-cols-6 gap-6">
-                            <div class="col-span-6 sm:col-span-3">
-                                <label for="client_name" class="input-label">{{ ctrans('texts.name') }}</label>
-                                <input id="client_name" class="input w-full" name="name" wire:model.defer="name"/>
-                                @error('name')
-                                <div class="validation validation-fail">
-                                    {{ $message }}
-                                </div>
-                                @enderror
+                <div class="grid grid-cols-6 gap-8">
+                    <div class="col-span-6 sm:col-span-3 relative">
+                        <label for="client_name"
+                            class="input-label absolute top-2 left-4 text-gray-500 text-sm">{{ ctrans('texts.name') }}</label>
+                        <input id="client_name"
+                            class="w-full bg-gray-200 border-0 p-4 pt-8 transition duration-200 focus:outline-none focus:ring-4 focus:ring-purple-300"
+                            name="name" wire:model.defer="name" />
+                        @error('name')
+                            <div class="validation validation-fail">
+                                {{ $message }}
                             </div>
-
-                            <div class="col-span-6 sm:col-span-3">
-                                <label for="client_vat_number"
-                                       class="input-label">{{ ctrans('texts.vat_number') }}</label>
-                                <input id="client_vat_number" class="input w-full" name="vat_number"
-                                       wire:model.defer="vat_number"/>
-                                @error('vat_number')
-                                <div class="validation validation-fail">
-                                    {{ $message }}
-                                </div>
-                                @enderror
-                            </div>
-
-                            <div class="col-span-6 sm:col-span-3">
-                                <label for="client_phone" class="input-label">{{ ctrans('texts.phone') }}</label>
-                                <input id="client_phone" class="input w-full" name="phone" wire:model.defer="phone"/>
-                                @error('phone')
-                                <div class="validation validation-fail">
-                                    {{ $message }}
-                                </div>
-                                @enderror
-                            </div>
-
-                            <div class="col-span-6 sm:col-span-3">
-                                <div class="inline-flex items-center">
-                                    <label for="client_website"
-                                           class="input-label">{{ ctrans('texts.website') }}</label>
-                                    <span class="text-xs ml-2 text-gray-600">E.g. https://example.com</span>
-                                </div>
-                                <input id="client_website" class="input w-full" name="website"
-                                       wire:model.defer="website"/>
-                                @error('website')
-                                <div class="validation validation-fail">
-                                    {{ $message }}
-                                </div>
-                                @enderror
-                            </div>
-                        </div>
+                        @enderror
                     </div>
-                    <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                        <button class="button button-primary bg-primary">{{ $saved }}</button>
+
+                    <div class="col-span-6 sm:col-span-3 relative">
+                        <label for="client_vat_number"
+                            class="input-label absolute top-2 left-4 text-gray-500 text-sm">{{ ctrans('texts.vat_number') }}</label>
+                        <input id="client_vat_number"
+                            class="w-full bg-gray-200 border-0 p-4 pt-8 transition duration-200 focus:outline-none focus:ring-4 focus:ring-purple-300"
+                            name="vat_number" wire:model.defer="vat_number" />
+                        @error('vat_number')
+                            <div class="validation validation-fail">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
+
+                    <div class="col-span-6 sm:col-span-3 relative">
+                        <label for="client_phone"
+                            class="input-label absolute top-2 left-4 text-gray-500 text-sm">{{ ctrans('texts.phone') }}</label>
+                        <input id="client_phone"
+                            class="w-full bg-gray-200 border-0 p-4 pt-8 transition duration-200 focus:outline-none focus:ring-4 focus:ring-purple-300"
+                            name="phone" wire:model.defer="phone" />
+                        @error('phone')
+                            <div class="validation validation-fail">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+
+                    <div class="col-span-6 sm:col-span-3 relative">
+                        <label for="client_website"
+                            class="input-label absolute top-2 left-4 text-gray-500 text-sm">{{ ctrans('texts.website') }}</label>
+                        <input id="client_website"
+                            class="w-full bg-gray-200 border-0 p-4 pt-8 transition duration-200 focus:outline-none focus:ring-4 focus:ring-purple-300"
+                            name="website" wire:model.defer="website" />
+                        @error('website')
+                            <div class="validation validation-fail">
+                                {{ $message }}
+                            </div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="mt-8 text-right">
+                    <button class="button button-primary bg-primary">{{ $saved }}</button>
                 </div>
             </form>
         </div> <!-- End of right side -->
--- resources/views/portal/ninja2020/profile/settings/personal-address.blade.php
+++ resources/views/portal/ninja2020/profile/settings/personal-address.blade.php
@@ -1,79 +1,96 @@
-<div class="mt-10 sm:mt-6">
-    <div class="md:grid md:grid-cols-3 md:gap-6">
+<div class="mt-8">
+    <div>
         <div class="md:col-span-1">
             <div class="sm:px-0">
-                <h3 class="text-lg font-medium leading-6 text-gray-900">{{ ctrans('texts.billing_address') }}</h3>
+                <h3 class="text-3xl font-semibold leading-6 text-gray-900">{{ ctrans('texts.billing_address') }}</h3>
             </div>
         </div>
-        <div class="mt-5 md:mt-0 md:col-span-2">
+
+        <div class="mt-8">
             <form wire:submit.prevent="submit" method="POST" id="update_billing_address">
                 @csrf
-                <div class="px-4 py-5 bg-white sm:p-6">
-                    <div class="grid grid-cols-6 gap-6">
-                        <div class="col-span-6 sm:col-span-4">
-                            <label for="address1" class="input-label">{{ ctrans('texts.address1') }}</label>
-                            <input id="address1" class="input w-full {{ in_array('billing_address1', (array) session('missing_required_fields')) ? 'border border-red-400' : '' }}" name="address1" wire:model.defer="address1" />
-                            @error('address1')
+                <div class="grid grid-cols-6 gap-8">
+                    <div class="col-span-6 relative">
+                        <label for="address1"
+                            class="input-label absolute top-2 left-4 text-gray-500 text-sm">{{ ctrans('texts.address1') }}</label>
+                        <input id="address1"
+                            class="w-full bg-gray-200 border-0 p-4 pt-8 transition duration-200 focus:outline-none focus:ring-4 focus:ring-purple-300 {{ in_array('billing_address1', (array) session('missing_required_fields')) ? 'ring-4 ring-red-400' : '' }}"
+                            name="address1" wire:model.defer="address1" />
+                        @error('address1')
                             <div class="validation validation-fail">
                                 {{ $message }}
                             </div>
-                            @enderror
-                        </div>
-                        <div class="col-span-6 sm:col-span-3">
-                            <label for="address2" class="input-label">{{ ctrans('texts.address2') }}</label>
-                            <input id="address2" class="input w-full {{ in_array('billing_address2', (array) session('missing_required_fields')) ? 'border border-red-400' : '' }}" name="address2" wire:model.defer="address2" />
-                            @error('address2')
+                        @enderror
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 relative">
+                        <label for="address2"
+                            class="input-label absolute top-2 left-4 text-gray-500 text-sm">{{ ctrans('texts.address2') }}</label>
+                        <input id="address2"
+                            class="w-full bg-gray-200 border-0 p-4 pt-8 transition duration-200 focus:outline-none focus:ring-4 focus:ring-purple-300 {{ in_array('billing_address2', (array) session('missing_required_fields')) ? 'ring-4 ring-red-400' : '' }}"
+                            name="address2" wire:model.defer="address2" />
+                        @error('address2')
                             <div class="validation validation-fail">
                                 {{ $message }}
                             </div>
-                            @enderror
-                        </div>
-                        <div class="col-span-6 sm:col-span-3">
-                            <label for="city" class="input-label">{{ ctrans('texts.city') }}</label>
-                            <input id="city" class="input w-full {{ in_array('billing_city', (array) session('missing_required_fields')) ? 'border border-red-400' : '' }}" name="city" wire:model.defer="city" />
-                            @error('city')
+                        @enderror
+                    </div>
+                    <div class="col-span-6 sm:col-span-3 relative">
+                        <label for="city"
+                            class="input-label absolute top-2 left-4 text-gray-500 text-sm">{{ ctrans('texts.city') }}</label>
+                        <input id="city"
+                            class="w-full bg-gray-200 border-0 p-4 pt-8 transition duration-200 focus:outline-none focus:ring-4 focus:ring-purple-300 {{ in_array('billing_city', (array) session('missing_required_fields')) ? 'ring-4 ring-red-400' : '' }}"
+                            name="city" wire:model.defer="city" />
+                        @error('city')
                             <div class="validation validation-fail">
                                 {{ $message }}
                             </div>
-                            @enderror
-                        </div>
-                        <div class="col-span-6 sm:col-span-2">
-                            <label for="state" class="input-label">{{ ctrans('texts.state') }}</label>
-                            <input id="state" class="input w-full {{ in_array('billing_state', (array) session('missing_required_fields')) ? 'border border-red-400' : '' }}" name="state" wire:model.defer="state" />
-                            @error('state')
+                        @enderror
+                    </div>
+                    <div class="col-span-6 sm:col-span-2 relative">
+                        <label for="state"
+                            class="input-label absolute top-2 left-4 text-gray-500 text-sm">{{ ctrans('texts.state') }}</label>
+                        <input id="state"
+                            class="w-full bg-gray-200 border-0 p-4 pt-8 transition duration-200 focus:outline-none focus:ring-4 focus:ring-purple-300 {{ in_array('billing_state', (array) session('missing_required_fields')) ? 'ring-4 ring-red-400' : '' }}"
+                            name="state" wire:model.defer="state" />
+                        @error('state')
                             <div class="validation validation-fail">
                                 {{ $message }}
                             </div>
-                            @enderror
-                        </div>
-                        <div class="col-span-6 sm:col-span-2">
-                            <label for="postal_code" class="input-label">{{ ctrans('texts.postal_code') }}</label>
-                            <input id="postal_code" class="input w-full {{ in_array('billing_postal_code', (array) session('missing_required_fields')) ? 'border border-red-400' : '' }}" name="postal_code" wire:model.defer="postal_code" />
-                            @error('postal_code')
+                        @enderror
+                    </div>
+                    <div class="col-span-6 sm:col-span-2 relative">
+                        <label for="postal_code"
+                            class="input-label absolute top-2 left-4 text-gray-500 text-sm">{{ ctrans('texts.postal_code') }}</label>
+                        <input id="postal_code"
+                            class="w-full bg-gray-200 border-0 p-4 pt-8 transition duration-200 focus:outline-none focus:ring-4 focus:ring-purple-300 {{ in_array('billing_postal_code', (array) session('missing_required_fields')) ? 'ring-4 ring-red-400' : '' }}"
+                            name="postal_code" wire:model.defer="postal_code" />
+                        @error('postal_code')
                             <div class="validation validation-fail">
                                 {{ $message }}
                             </div>
-                            @enderror
-                        </div>
-                        <div class="col-span-6 sm:col-span-2">
-                            <label for="country" class="input-label">@lang('texts.country')</label>
-                            <select id="country" class="input w-full form-select bg-white {{ in_array('billing_country', (array) session('missing_required_fields')) ? 'border border-red-400' : '' }}" wire:model.defer="country_id">
-                                <option value="none"></option>
-                                @foreach($countries as $country)
-                                    <option value="{{ $country->id }}">
-                                        {{ $country->iso_3166_2 }} ({{ $country->name }})
-                                    </option>
-                                @endforeach
-                            </select>
-                            @error('country')
+                        @enderror
+                    </div>
+                    <div class="col-span-6 sm:col-span-2 relative">
+                        <label for="country"
+                            class="input-label absolute top-2 left-4 text-gray-500 text-sm">@lang('texts.country')</label>
+                        <select id="country"
+                            class="w-full bg-gray-200 border-0 p-4 pt-8 transition duration-200 focus:outline-none focus:ring-4 focus:ring-purple-300 form-select rounded-none {{ in_array('billing_country', (array) session('missing_required_fields')) ? 'ring-4 ring-red-400' : '' }}"
+                            wire:model.defer="country_id">
+                            <option value="none"></option>
+                            @foreach ($countries as $country)
+                                <option value="{{ $country->id }}">
+                                    {{ $country->iso_3166_2 }} ({{ $country->name }})
+                                </option>
+                            @endforeach
+                        </select>
+                        @error('country')
                             <div class="validation validation-fail">
                                 {{ $message }}
                             </div>
-                            @enderror
-                        </div>
+                        @enderror
                     </div>
                 </div>
-                <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                <div class="mt-8 text-right">
                     <button class="button button-primary bg-primary">{{ $saved }}</button>
                 </div>
         </div>
